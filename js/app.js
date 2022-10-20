ab531825457d2866662282cc58d5448c57796354
Seyyed Mohammad Hossein Tabatabaeifard <mohammadirani469@gmail.com>
2022-10-20 13:51:10 +0330

    added recording page

--- js/app.js
+++ js/app.js
@@ -1,16 +1,16 @@
-const username = document.querySelector("#username");
+const userName = document.querySelector("#username");
 const submit = document.querySelector("#submit");
 const error = document.querySelector("#error");
-
-
-submit.addEventListener("click",(e) => {
-    e.preventDefault();
-    if (username.value === "") {
-        username.style.border = "1px solid red";
-        error.style.display = "block";
-    }else{
-        username.style.border = "1px solid gray";
-        error.style.display = "none";
-        location.href = `https://live.pentaserver.ir/tabatabaei?guestname=${username.value}`
-    }
-})
+submit.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (userName.value === "") {
+    userName.style.border = "1px solid red";
+    error.style.display = "block";
+  } else {
+    userName.style.border = "1px solid gray";
+    error.style.display = "none";
+    nameOfUser = userName.value;
+   
+     location.href = `https://live.pentaserver.ir/tabatabaei?guestname=${nameOfUser}`;
+  }
+});
